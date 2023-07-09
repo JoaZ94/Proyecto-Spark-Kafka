6da5824c04592477323894f78f28d6350ab8fb96
JoaZ94 <ingjosediaz94@gmail.com>
2023-07-09 17:06:00 -0500

    Proyecto terminado

--- code/notification.py
+++ code/notification.py
@@ -1,7 +1,10 @@
+# Se importan las librerías necesarias
+
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 import pyspark.sql.functions as F
 
+# Función que obtiene el nombre, telefono y correo del cliente, así como el producto visto ál menos 30 segundos
 def clientes_notificar(df):
     df.createOrReplaceTempView("vProductosVistos")
 
@@ -22,6 +25,7 @@ def enviar_notificacion(nombre, productovisto): # Simula el envío de notificaci
 
 if __name__ == "__main__":
 
+    # Se crea una Sesión de Spark
     spark = SparkSession\
         .builder\
         .appName("KafkaIntegration")\
@@ -29,13 +33,15 @@ if __name__ == "__main__":
         .config("spark.sql.shuffle.partitions", 3)\
         .getOrCreate()
 
+    # Se crea un DataFrame con la información que se recibe de Kafka
     streaming_df = spark.readStream\
         .format("kafka")\
         .option("kafka.bootstrap.servers", "kafka1-p:9092, kafka2-p:9092, kafka3-p:9092")\
         .option("subscribe", "DataTopic")\
         .option("startingOffsets", "earliest")\
         .load()
-        
+
+    # Se crea el eschema de la data recibida    
     schema = StructType([
         StructField("id", StringType()),
         StructField("nombre", StringType()),
@@ -52,15 +58,18 @@ if __name__ == "__main__":
         StructField("taps", FloatType())
     ])
     
+    # Se genera una columna por cada clave
     parsed_df = streaming_df\
         .select(F.col("value").cast(StringType()).alias("value"))\
         .withColumn("input", F.from_json("value", schema))\
         .select("input.*", "input.ubicacion.*")\
         .drop("ubicacion")
 
+    # Se llama la función ''clientes-notificar
     clientes_notificados_df = clientes_notificar(parsed_df)
 
-    # Definir la consulta de escritura en archivo Parquet
+    # Definir la consulta en modo Append (para notificar solo a los nuevos registros)
+    # Se ejecuta la consulta cada 9 segundos
     query = clientes_notificados_df.writeStream\
         .outputMode("append") \
         .trigger(processingTime="9 seconds") \
